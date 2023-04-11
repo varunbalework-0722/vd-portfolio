628053247a2aaa439fe97a732df9a85a0f14dc32
Bhanuprakash-Depani <67039723+Bhanuprakash-Depani@users.noreply.github.com>
2023-04-11 16:33:40 +0530

    Add files via upload
    App folder

---
+++ app/head.tsx
@@ -0,0 +1,47 @@
+export default function Head() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="I am Bhanuprakash Depani, a frontend developer based in Bangalore, India. I'm a 2014 graduate from Sir Vishveswaraiah Institute of Science and Technology (aka Viswam College), Angallu, Andhra Pradesh. I'm passionate about creating beautiful, functional and user-friendly websites and applications, and I'm constantly pushing myself to learn and grow as a developer."
+      />
+      <meta
+        name="keywords"
+        content="bhanuprakash depani, portfolio, bhanuprakash, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio"
+      />
+      <meta
+        property="og:site_name"
+        content="Bhanuprakash Prakash's Portfolio"
+      />
+      <meta
+        property="og:title"
+        content="Portfolio | Bhanuprakash Depani - Frontend Developer"
+      />
+      <meta
+        property="og:description"
+        content="I am Bhanuprakash Depani, a frontend developer based in Bangalore, India. I'm a 2014 graduate from Sir Vishveswaraiah Institute of Science and Technology (aka Viswam College), Angallu, Andhra Pradesh. I'm passionate about creating beautiful, functional and user-friendly websites and applications, and I'm constantly pushing myself to learn and grow as a developer."
+      />
+      <meta property="og:url" content="https://jigarsable.vercel.app" />
+      <meta
+        property="og:image"
+        content="https://jigarsable.vercel.app/portfolio-fork.png"
+      />
+      <link rel="canonical" href="https://jigarsable.vercel.app" />
+      <meta name="author" content="Bhanuprakash Depani" />
+      <meta name="language" content="English" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="https://jigarsable.vercel.app" />
+      <meta
+        name="application-name"
+        content="Portfolio | Bhanuprakash Depani- Full Stack Developer"
+      />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Portfolio | Bhanuprakash Depani- Full Stack Developer"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Portfolio | Bhanuprakash Depani- Frontend Developer</title>
+    </>
+  );
+}
